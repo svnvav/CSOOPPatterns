ca4db96de2856f34305bf8ab00d5abd1fda8abd4
svnvav <svnvav@mail.ru>
2020-03-13 16:59:50 +0300

    Memento in progress

---
+++ CSOOPPatterns/Behavioral/Memento/Hero.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CSOOPPatterns.Behavioral.Memento
+{
+    public class Hero
+    {
+        public struct State
+        {
+            public float x, y, z;
+        }
+        
+        public class Memento
+        {
+            private State _state;
+
+            public State State => _state;
+
+            public Memento(Hero hero)
+            {
+                _state = hero._state;
+            }
+        }
+
+        private State _state;
+
+        public Memento Save()
+        {
+            return new Memento(this);
+        }
+
+        public void Restore(Memento memento)
+        {
+            _state = memento.State;
+        }
+
+        public void Move()
+        {
+            _state.x += 1;
+            _state.y += 2;
+            _state.z += 3;
+        }
+    }
+}
--- CSOOPPatterns/Behavioral/Memento/MementoClient.cs
+++ CSOOPPatterns/Behavioral/Memento/MementoClient.cs
@@ -1,10 +1,14 @@
-﻿namespace CSOOPPatterns.Behavioral.Memento
+﻿using System.Collections.Generic;
+
+namespace CSOOPPatterns.Behavioral.Memento
 {
     public class MementoClient
     {
         public void DoStuff()
         {
-            
+            var hero = new Hero();
+            var saves = new List<Hero.Memento>();
+            //TODO:
         }
     }
 }
